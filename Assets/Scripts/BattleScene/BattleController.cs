e08ac223e57a638ded100da807e1bff85b918503
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2017-01-08 21:34:51 +0900

    smallEnemy, bigEnemy, bossEnemyのPrefabを追加
    BatteleScene生成時にEnemyを作成する機能を追加

---
+++ Assets/Scripts/BattleScene/BattleController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleController : MonoBehaviour {
+	public GameObject smallEnemy;
+	public GameObject bigEnemy;
+	public GameObject bossEnemy;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (smallEnemy);
+		Instantiate (bigEnemy);
+		Instantiate (bossEnemy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
