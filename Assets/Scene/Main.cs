c13b932ba89d112377f41a15e9f169798e1cf986
Dagashiya1995 <touroku146749@gmail.com>
2017-01-09 01:07:54 +0900

    add enemy class

---
+++ Assets/Scene/Main.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class Main : MonoBehaviour {    public Enemy enemy;	void Start () {	}		void Update () {        if (enemy != null)
+        {
+            Debug.Log("hoge");
+            enemy.reciveDamage(20);            if (enemy.deadOrLive() == false)            {                enemy.vanish();            }
+        }	}}
