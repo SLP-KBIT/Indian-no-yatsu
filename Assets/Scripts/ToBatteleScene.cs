6f1751a59deff6ef0f04b7430994f07b782a9fd3
nobarudo <s13t203@stmail.eng.kagawa-u.ac.jp>
2017-01-08 17:01:07 +0900

    add StartScene

---
+++ Assets/Scripts/ToBatteleScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToBatteleScene : MonoBehaviour {
+
+	public void ToBattele()
+    {
+        Application.LoadLevel("BatteleScene");
+    }
+}
