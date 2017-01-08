c3f8efad7e0e66b614ef141dae4b4617ea006222
gembaf <gembaf.github@gmail.com>
2017-01-08 22:57:21 +0900

    スペースを押すとスライディングする

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,20 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
 	private static Vector2 SPEED = new Vector2(0.1f, 0.15f);	// 速度
+	private Animator anim;
 
 	// Use this for initialization
 	void Start ()
 	{
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		anim.SetBool("isSlide", false);
 		Move ();
+		Slide ();
 	}
 
 	// 移動関数
@@ -39,4 +43,14 @@ public class Player : MonoBehaviour
 		transform.position = position;
 	}
 
+	void Slide ()
+	{
+		if ( !Input.GetKey("space") ) { return; }
+
+		anim.SetBool("isSlide", true);
+		Vector2 position = transform.position;
+		float rotationX = transform.localRotation.y == 0 ? 1 : -1;
+		Vector2 endPosition = new Vector2 (position.x + 0.5f * rotationX, position.y);
+		transform.position = endPosition;
+	}
 }
